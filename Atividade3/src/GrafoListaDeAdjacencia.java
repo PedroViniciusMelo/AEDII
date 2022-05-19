b170f1f060adcc9445d442ff3e8994c61ad851ca
Pedro Vinícius de Melo Silva <65138009+PedroViniciusMelo@users.noreply.github.com>
2022-05-19 15:10:42 -0300

    Ajuste de erro

--- Atividade3/src/GrafoListaDeAdjacencia.java
+++ Atividade3/src/GrafoListaDeAdjacencia.java
@@ -135,49 +135,6 @@ public class GrafoListaDeAdjacencia {
 
     }
 
-    /*public void DFS(){
-        this.cor = new cores[this.quantidadeDeVertices];
-        this.parents = new Character[this.quantidadeDeVertices];
-        this.descoberta = new Integer[this.quantidadeDeVertices];
-        this.distanciaFinal = new Integer[this.quantidadeDeVertices];
-
-        int hold = 0;
-        for (char item: this.vertices) {
-            this.cor[hold] = cores.BRANCO;
-            this.parents[hold] = null;
-            this.descoberta[hold] = 0;
-            this.distanciaFinal[hold] = 0;
-            hold++;
-        }
-
-        this.clock = 0;
-
-        for (char item : this.vertices) {
-            int u = this.vertices.indexOf(item);
-            if(cor[u].equals(cores.BRANCO)){
-                DFS_VISIT(u);
-            }
-        }
-
-        printItems();
-    }*/
-
-    /*private void DFS_VISIT(int u) {
-        this.cor[u] = cores.CINZA;
-        this.clock++;
-        this.descoberta[u] = this.clock;
-
-        for (int v : this.listaDeAdjacencia[u]){
-            if(cor[v].equals(cores.BRANCO)){
-                this.parents[v] = this.vertices.get(u);
-                DFS_VISIT(v);
-            }
-        }
-        cor[u] = cores.PRETO;
-
-        distanciaFinal[u] = ++this.clock;
-    }*/
-
     void printItems(){
         System.out.println("Cores: " + Arrays.toString(this.cor));
         System.out.println("Distâncias: " + Arrays.toString(this.descoberta));
